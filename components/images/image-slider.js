804beb649dc963012b6dab02a6451466be45628d
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-03-09 15:30:18 +0000

    feat: import images and register in images array

--- components/images/image-slider.js
+++ components/images/image-slider.js
@@ -1,3 +1,39 @@
+import Image from "next/image"
+
+import burgerImg from '@/assets/burger.jpg'
+import curryImg from '@/assets/curry.jpg'
+import dumplingImg from '@/assets/dumplings.jpg'
+import macncheeseImg from '@/assets/macncheese.jpg'
+import pizzaImg from '@/assets/pizza.jpg'
+import schnitzelImg from '@/assets/schnitzel.jpg'
+import tomatoSaladImg from '@/assets/tomatosalad.jpg'
+
+import classes from './image-slider.module.css'
+
+const images = [
+  {
+    src: burgerImg, alt: 'A delicious burger with lettuce, tomato, and cheese',
+  },
+  {
+    src: curryImg, alt: 'A bowl of curry with rice and vegetables',
+  },
+  {
+    src: dumplingImg, alt: 'A plate of dumplings with dipping sauce',
+  },
+  {
+    src: macncheeseImg, alt: 'A bowl of mac and cheese with a crispy topping',
+  },
+  {
+    src: pizzaImg, alt: 'A slice of pizza with pepperoni and cheese',
+  },
+  {
+    src: schnitzelImg, alt: 'A plate of schnitzel with potatoes and vegetables',
+  },
+  {
+    src: tomatoSaladImg, alt: 'A bowl of tomato salad with basil and mozzarella',
+  },
+]
+
 const ImageSlider = () => {
   return (
     <div>image-slider</div>
