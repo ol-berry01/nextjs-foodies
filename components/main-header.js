3b627b5eaa8da1b7939a602120295859a9ffe103
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-03-09 10:06:23 +0000

    feat: add main-header.module
    add main-header styles as nextjs css module

--- components/main-header.js
+++ components/main-header.js
@@ -1,17 +1,18 @@
 import Link from "next/link"
 
-import logoImg from '../assets/logo.png'
+import classes from './main-header.module.css'
+import logoImg from '@/assets/logo.png'
 
 const MainHeader = () => {
   return (
-    <header>
+    <header className={ classes.header }>
 
-      <Link href={ '/' }>
+      <Link className={ classes.logo } href={ '/' }>
         <img src={ logoImg.src } alt="A plate with food on it" />
         NextLevel Food
       </Link>
 
-      <nav>
+      <nav className={ classes.nav }>
         <ul>
           <li>
             <Link href={ '/meals' }>Browse Meals</Link>
