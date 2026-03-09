b0b1e0e95cd03d43043e092f7fbe5afd75643930
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-03-09 13:37:53 +0000

    feat: add static header elements
    add hero text and description along with CTA links

--- app/page.js
+++ app/page.js
@@ -8,8 +8,14 @@ export default function Home() {
       <header className={ classes.header }>
         <div className={ classes.slideshow }></div>
         <div>
-          <div className={ classes.hero }></div>
-          <div className={ classes.cta }></div>
+          <div className={ classes.hero }>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste & share food from all over the world</p>
+          </div>
+          <div className={ classes.cta }>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
         </div>
       </header>
       <main>
