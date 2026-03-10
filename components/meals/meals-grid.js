cda0fc8bbb5c9a2fb5bd4309e4e3b930016a225d
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-03-10 09:45:55 +0000

    feat: output MealsItem into MealsGrid component

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,9 +1,13 @@
+import MealsItem from './meal-item'
+
 import classes from './meals-grid.module.css'
 
 const MealsGrid = ( { meals } ) => {
   return (
     <ul className={ classes.meals }>
-      { meals.map( meal => <li key={ meal.id }></li> ) }
+      { meals.map( meal => <li key={ meal.id }>
+        <MealsItem { ...meal } />
+      </li> ) }
     </ul>
   )
 }
