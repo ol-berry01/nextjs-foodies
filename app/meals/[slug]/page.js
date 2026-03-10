da258530f31a3e75eae7fc27dd813f0e853546d5
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-03-10 15:15:45 +0000

    feat: add meals/slug/page.js header
    set header markup with placeholder tags, import classes from page module css

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,6 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import classes from './page.module.css'
+
 const MealsSlug = () => {
   return (
-    <main>MealsSlug</main>
+    <>
+      <header className={classes.header}>
+        <div className={ classes.image }>
+          <Image fill />
+        </div>
+        <div className={ classes.headerText }>
+          <h1>TITLE</h1>
+          <p className={ classes.creator }>
+            by <Link href={ `mailto:${ 'email'}` }>NAME</Link>
+          </p>
+          <p className={ classes.summary }>SUMMARY</p>
+        </div>
+      </header>
+      <main>MealsSlug</main>
+    </>
   )
 }
 
