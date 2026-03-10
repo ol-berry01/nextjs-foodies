a14469e3874b51e8cbdbc8fbbf61c877d1e0eec3
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-03-10 11:26:56 +0000

    feat: handle 404 errors

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <main className='not-found'>
+      <h1>NotFound</h1>
+      <p>We could not find the requested page or resource</p>
+    </main>
+  )
+}
+
+export default NotFound
