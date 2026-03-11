cbd26e61a2b9585a7c35e0714dc9e4e26731f193
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-03-11 08:56:08 +0000

    feat: add static markup for share meals page
    include page module css

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,49 @@
-const MealsShare = () => {
+import classes from './page.module.css'
+
+const ShareMealPage = () => {
   return (
-    <main>MealsShare</main>
+    <>
+      <header className={ classes.main }>
+        <h1>Share your <span className={ classes.highlight }>favourite meal</span></h1>
+        <p>Or any other meal you feel needs sharing!</p>
+      </header>
+      <main className={ classes.main }>
+        <form className={ classes.form }>
+          <div className={ classes.row }>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input type="text" name="name" id="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">Your email</label>
+              <input type="email" name="email" id="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Title</label>
+            <input type="text" id='title' name='title' required />
+          </p>
+          <p>
+            <label htmlFor="summary">Summary</label>
+            <input id='summary' name='summary' required />
+          </p>
+          <p>
+            <label htmlFor="instructions">instructions</label>
+            <textarea 
+              id='instructions' 
+              name='instructions' 
+              rows='5' 
+              required
+            ></textarea>
+          </p>
+          Image picker
+          <p className={ classes.actions }>
+            <button type='submit'>Share Meal</button>
+          </p>
+        </form>
+      </main>
+    </>
   )
 }
 
-export default MealsShare
+export default ShareMealPage
