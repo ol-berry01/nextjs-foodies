1d5076fc8c2d1a899278cd56d79fa204e5f4e05c
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-03-11 08:59:29 +0000

    feat: create ImagePicker component
    add image-picker module css supporting file

---
+++ components/meals/image-picker.js
@@ -0,0 +1,9 @@
+import classes from './image-picker.module.css'
+
+const ImagePicker = () => {
+  return (
+    <div className={ classes.picker }>ImagePicker</div>
+  )
+}
+
+export default ImagePicker
