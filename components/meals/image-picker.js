3616c28815dde8ed0bd55c247dd27cb96ef4f148
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-03-11 09:02:55 +0000

    feat: set ImagePicker form fields
    include component props

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,8 +1,18 @@
 import classes from './image-picker.module.css'
 
-const ImagePicker = () => {
+const ImagePicker = ( { label, name } ) => {
   return (
-    <div className={ classes.picker }>ImagePicker</div>
+    <div className={ classes.picker }>
+      <label htmlFor={ name }>{ label }</label>
+      <div className={ classes.controls }>
+        <input 
+          type="file" 
+          id={ name } 
+          name={ name } 
+          accept="image/png, image/jpeg" 
+        />
+      </div>
+    </div>
   )
 }
 
