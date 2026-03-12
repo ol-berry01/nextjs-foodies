0d3d4cbc9ed86fd9f72e9691f0402fc3a59a6b5f
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-03-12 10:40:17 +0000

    feat: add basic validation for meal share fields

---
+++ app/meals/share/error.js
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+
+const Error = ( { error } ) => {
+  return (
+    <main className="error">
+      <h1>An error has occurred!</h1>
+      <p>Failed to create meal</p>
+    </main>
+  )
+}
+
+export default Error
